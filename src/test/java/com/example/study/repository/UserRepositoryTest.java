004ab1fe6b43915f7f86c4874e39bf50fcba941b
Gyumin-Kimm <kkyumink@gmail.com>
2021-03-23 18:30:23 +0900

    JPA CRUD - read & update

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -28,14 +29,28 @@ public class UserRepositoryTest extends StudyApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
+    @Test
     public void read(){
-
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+            System.out.println("user :" + selectUser);
+            System.out.println("email : " + selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser ->{
+           selectUser.setAccount("PPPP");
+           selectUser.setUpdatedAt(LocalDateTime.now());
+           selectUser.setUpdatedBy("update method()");
 
+           userRepository.save(selectUser);
+        });
     }
 
+    @Test
     public void delete(){
 
     }
